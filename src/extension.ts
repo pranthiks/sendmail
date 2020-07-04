77f1b64a308c2c0a0b88f1b8de5f5de94099e297
Pranthik Samal <pranthiksamal@Pranthiks-iMac.local>
2020-07-03 17:26:13 -0700

    working example of how to execute a shell command

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import { stdout, stderr } from 'process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,20 +8,27 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "emailer" is now active!');
+	console.log('Extension "emailer" is now active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('emailer.helloWorld', () => {
+	// TODO: Start postfix here
+
+	const disposable = vscode.commands.registerCommand('emailer.sendEmail', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Emailer!');
+		cp.exec('ls', (err, stdout, stderr) => {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (err) {
+				console.log('error: ' + err);
+			}
+		});
+
+		vscode.window.showInformationMessage('Email sent!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	// TODO: Turn off postfix here
+}
