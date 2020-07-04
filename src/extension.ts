3359340383ff7099c2000d243e1fff8b2929704d
Pranthik Samal <pranthik.samal@gmail.com>
2020-07-04 16:33:47 -0700

    clean up variable naming

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,22 @@ import * as cp from 'child_process';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const disposable = vscode.commands.registerCommand('emailer.sendEmail', () => {
-		const filePath = vscode.window.activeTextEditor?.document.fileName;
-		const emailCommand = `cat ${filePath} | sendmail -vt`;
-		console.log(`Running sendmail command: ${emailCommand}`);
-		cp.exec(emailCommand, (error, stdout, stderr) => {
-      if (error) {
+	const command = "emailer.sendEmail";
+	const commandHandler = () => {
+		const emailFilePath = vscode.window.activeTextEditor?.document.fileName;
+		const emailCommand = `cat ${emailFilePath} | sendmail -vt`;
+		cp.exec(emailCommand, (error, _stdout, stderr) => {
+			if (error) {
 				vscode.window.showErrorMessage(`Error: ${stderr}`);
 				console.error(`error: ${error}`);
 				return;
 			}
 
 			vscode.window.showInformationMessage('Email sent!');
-    });
+		});
+	};
 
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand(command, commandHandler));
 }
 
 export function deactivate() {}
